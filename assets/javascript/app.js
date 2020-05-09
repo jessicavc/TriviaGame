95a00b2a39b4e3d860988ca67f038e854aa3d05d
Jessica VC <jessicavc@this-guy-ii.home>
2020-05-09 14:30:00 -0700

    updated questions after successfully getting 10 questions on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,19 +39,19 @@ var questions = [{
     answer: 0
 
 }, {
-    question: "Nichelle Nichols plays Lt. Nyota Uhara in The Original Series and the films that follow. Nichols nearly left the show in the 1960's but was convinced to stay because of the social impacts Uhara had as a female, African American bridge officer. Who convinced Nichols to stay?",
-    answerList: ["Gene Roddenberry", "Martin Luther King, Jr", "Malcolm X", "Leonard Nemoy"],
-    answer: 1
+    question: "Data is a unique android with a strong desire to better understand human nature. Data decides to have a child he names Lal. He does the unthinkable and allows Lal to choose its own gender and appearance. What does Lal choose?",
+    answerList: ["Human Female", "Klingon Male", "Klingon Female", "Betazoid Female"],
+    answer: 0
 
 }, {
-    question: "Nichelle Nichols plays Lt. Nyota Uhara in The Original Series and the films that follow. Nichols nearly left the show in the 1960's but was convinced to stay because of the social impacts Uhara had as a female, African American bridge officer. Who convinced Nichols to stay?",
-    answerList: ["Gene Roddenberry", "Martin Luther King, Jr", "Malcolm X", "Leonard Nemoy"],
-    answer: 1
+    question: "Starfleet Academy trains new cadets with a simulation of a no-win scenario called: (HINT: Stacy gave us the answer in class)",
+    answerList: ["Darmok and Jalad at Tanagra", "Q's Paradise", "Tomagachi Maru", "Kobiyashi Maru"],
+    answer: 3
 }, {
 
-    question: "Nichelle Nichols plays Lt. Nyota Uhara in The Original Series and the films that follow. Nichols nearly left the show in the 1960's but was convinced to stay because of the social impacts Uhara had as a female, African American bridge officer. Who convinced Nichols to stay?",
-    answerList: ["Gene Roddenberry", "Martin Luther King, Jr", "Malcolm X", "Leonard Nemoy"],
-    answer: 1
+    question: "Majel Barrett, married to Star Trek creator Gene Roddenberry, is appropriately known as the Mother of Star Trek. She has played a variety of characters throughout the Star Trek universe, including Nurse Chapel and Lwaxana Troi. What was her first Trek appearance? (HINT: Currently played by Rebecca Romijn on Star Trek: Discovery)",
+    answerList: ["Nurse Chapel", "Computer", "Number One", "Doctor Pulaski"],
+    answer: 2
 }];
 
 $("#start").on("click", function () {
