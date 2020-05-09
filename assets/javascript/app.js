4a6c9f0d9ab108403602341898d27bf4144caa8d
Jessica VC <jessicavc@this-guy-ii.home>
2020-05-09 14:53:07 -0700

    experimental. changed timer from 60 to 90

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var timer = 60;
+var timer = 90;
 
 var intervalId;
 
@@ -61,7 +61,7 @@ $("#start").on("click", function () {
     $(this).hide();
 
     // Display initial time countdown
-    $("#timer").html("<h3>Time Remaining: 60 Seconds</h3>" + "<br>");
+    $("#timer").html("<h3>Time Remaining: 90 Seconds</h3>" + "<br>");
 
     // Start timer countdown
     run();
